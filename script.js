9fab7399b58376398c45091cab88128ca46eee91
Mateus <mateus.bohn.tb@gmail.com>
2026-04-13 22:08:45 -0300

    att

---
+++ script.js
@@ -0,0 +1,69 @@
+function Atividade1(){
+
+
+let contador=0
+let numero=3
+let texto =""
+while (contador<=30) {
+let resultado=numero*contador 
+texto += numero +"x"+contador+"="+resultado+"\n"
+contador=contador+3
+}
+alert (texto)
+
+//algoritmo "Atividade 1"
+
+//var
+ // numero, contador ,resultado : inteiro
+
+//inicio
+
+  //contador<-0
+ // numero <-3
+  //enquanto contador <=30 faca
+   // resultado <-numero*contador
+   // escreval(numero "x",contador,"=",resultado)
+    //contador<- contador +1
+ // fimenquanto
+//fimalgoritmo
+
+}
+const BonusAtividade1 = document.getElementById("Atividade1")
+BonusAtividade1.addEventListener("click", () => { Atividade1() })
+
+function Atividade2(){
+
+}
+const BonusAtividade2 = document.getElementById("Atividade2")
+BonusAtividade2.addEventListener("click", () => { Atividade2() })
+
+function Atividade3(){
+
+}
+const BonusAtividade3 = document.getElementById("Atividade3")
+BonusAtividade3.addEventListener("click", () => { Atividade3() })
+
+function Atividade4(){
+
+}
+const BonusAtividade4 = document.getElementById("Atividade4")
+BonusAtividade4.addEventListener("click", () => { Atividade4() })
+
+function Atividade5(){
+
+}
+const BonusAtividade5 = document.getElementById("Atividade5")
+BonusAtividade5.addEventListener("click", () => { Atividade5() })
+
+function Atividade6(){
+
+}
+const BonusAtividade6 = document.getElementById("Atividade6")
+BonusAtividade6.addEventListener("click", () => { Atividade6() })
+
+function Atividade7(){
+
+}
+const BonusAtividade7 = document.getElementById("Atividade7")
+BonusAtividade7.addEventListener("click", () => { Atividade7() })
+
